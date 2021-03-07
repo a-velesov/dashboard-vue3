a2c97d91ffc66a5123bf4e02546695c73e648feb
Alexsandr <alexnem@inbox.ru>
2021-03-07 22:44:50 +0300

    added request form in modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,9 @@
     </request-table>
 
     <teleport to="body">
-      <app-modal v-if="modal" title="Создать заявку" @close="modal = false"></app-modal>
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false">
+        <request-modal @created="modal = false"></request-modal>
+      </app-modal>
     </teleport>
   </app-page>
 </template>
@@ -21,10 +23,13 @@ import { ref } from 'vue';
 import AppPage from '@/components/ui/AppPage.vue';
 import RequestTable from '@/components/request/RequestTable.vue';
 import AppModal from '@/components/ui/AppModal.vue';
+import RequestModal from '@/components/request/RequestModal.vue';
 
 export default {
   name: 'Home',
-  components: { AppModal, RequestTable, AppPage },
+  components: {
+    RequestModal, AppModal, RequestTable, AppPage,
+  },
   setup() {
     const modal = ref(false);
 
