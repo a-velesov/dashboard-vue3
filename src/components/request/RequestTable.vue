38b7284fac1678012ba8f4522f2bc7699ff50e18
Alexsandr <alexnem@inbox.ru>
2021-03-07 18:10:55 +0300

    added modal

---
+++ src/components/request/RequestTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <h4 v-if="requests.length === 0" class="text-center">Заявок пока нет</h4>
+  <table v-else class="table">
+    <thead>
+    <tr>
+      <th>#</th>
+      <th>ФИО</th>
+      <th>Телефон</th>
+      <th>Сумма</th>
+      <th>Статус</th>
+      <th>Действие</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+  </table>
+</template>
+
+<script>
+
+export default {
+  props: ['requests'],
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,36 @@
 <template>
   <app-page title="Список заявок">
     <template #header>
-      <button class="btn primary">Создать</button>
+      <button
+        class="btn primary"
+        @click="modal = true"
+      >Создать</button>
     </template>
+
+    <request-table :requests="[]">
+    </request-table>
+
+    <teleport to="body">
+      <app-modal v-if="modal" title="Создать заявку" @close="modal = false"></app-modal>
+    </teleport>
   </app-page>
 </template>
 
 <script>
+import { ref } from 'vue';
 import AppPage from '@/components/ui/AppPage.vue';
+import RequestTable from '@/components/request/RequestTable.vue';
+import AppModal from '@/components/ui/AppModal.vue';
 
 export default {
   name: 'Home',
-  components: { AppPage },
+  components: { AppModal, RequestTable, AppPage },
+  setup() {
+    const modal = ref(false);
+
+    return {
+      modal,
+    };
+  },
 };
 </script>
