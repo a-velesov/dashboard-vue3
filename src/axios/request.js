0db33f5671b5065ca2272fc748fb3cfc33ea554c
Alexsandr <alexnem@inbox.ru>
2021-03-13 13:48:59 +0300

    added axios interceptors

--- src/axios/request.js
+++ src/axios/request.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
+// eslint-disable-next-line import/no-cycle
+import router from '@/router';
 
 const requestAxios = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
+});
+
+// Add a response interceptor
+requestAxios.interceptors.response.use(null, (error) => {
+  if (error.response.status === 401) {
+    router.push('/auth?message=auth');
+  }
+  return Promise.reject(error);
 });
 
 export default requestAxios;
