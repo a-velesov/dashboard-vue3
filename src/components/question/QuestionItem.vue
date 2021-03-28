67e3d59d69721552357ec0b95e133aeb0aceb3a4
Alexsandr <alexnem@inbox.ru>
2021-03-28 16:11:08 +0300

    added help page

---
+++ src/components/question/QuestionItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="question" @click="isOpen = !isOpen">
+    <div class="question-title">{{question.title}}</div>
+    <div class="question-body" v-if="isOpen">{{question.text}}</div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'QuestionItem',
+  props: ['question'],
+  setup() {
+    return {
+      isOpen: ref(false),
+    };
+  },
+};
+
+</script>
+
+<style lang='scss'>
+
+</style>
--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <app-page back title="Помощь">
+    <question-item
+      v-for="question in questions"
+      :key="question.title"
+      :question="question"
+    />
+  </app-page>
 </template>
+
+<script>
+import AppPage from '@/components/ui/AppPage.vue';
+import QuestionItem from '@/components/question/QuestionItem.vue';
+
+export default {
+  components: { QuestionItem, AppPage },
+  setup() {
+    const questions = [
+      { title: 'Что есть в данной системе?', text: 'Тут есть все, для того, чтобы смотреть, какие заявки человек добавлял' },
+      { title: 'Как тут работает авторизация?', text: 'Все реализовано через Firebase' },
+    ];
+
+    return { questions };
+  },
+};
+</script>
